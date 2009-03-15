fbf619d0fdb3e7c690ceebaa04d7cce7a30b98cb
Axel Liljencrantz <axel.liljencrantz@freecode.no>
2009-03-15 10:25:57 +0000

    Make users deletable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -87,4 +87,15 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(users_url) }
+      format.xml  { head :ok }
+    end
+  end
+
+
 end
