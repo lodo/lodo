bed608f42a79e4379156333e13f43b5da7fdeef6
Axel Liljencrantz <axel.liljencrantz@freecode.no>
2009-03-26 15:10:43 +0000

    Fix order javascript to use discounts correctly.
    refs 937

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,9 @@ class Order < ActiveRecord::Base
   belongs_to :transport
   belongs_to :company
   has_many :order_items
+
+  def discount
+    100.0 * (1.0-self.price / self.order_items.inject(0.0){|sum,item| sum+item.price})
+  end
+
 end
