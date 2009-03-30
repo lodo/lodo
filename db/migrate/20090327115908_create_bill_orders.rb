4fb1ffec317114887cb1cae411390dfb6116b435
Egil Moeller <egil.moller@freecode.no>
2009-03-30 10:55:44 +0200

    Fulhack

--- db/migrate/20090327115908_create_bill_orders.rb
+++ db/migrate/20090327115908_create_bill_orders.rb
@@ -1,7 +1,7 @@
 class CreateBillOrders < ActiveRecord::Migration
   def self.up
     create_table :bill_orders do |t|
-      t.references :bill_id
+      t.references :bill
 
       t.timestamps
     end
