8f8697a677a2a0826e89015a38dec250d0d8f3b7
Egil Moeller <egil.moller@freecode.no>
2009-04-01 16:39:01 +0200

    Input fields now get the right name and id

--- public/javascripts/bills.js
+++ public/javascripts/bills.js
@@ -1,5 +1,4 @@
 var bills = {
-
     getOrderPrice: function(id) 
     {
 	
@@ -15,6 +14,7 @@ var bills = {
 
     validate: function()
     {
+/*
 	for (var i=0; i < $('#bill')[0].table_lines; i++) {
 	    var price = $('#unitPrice_' + i)[0];
 	    var orderId = $('#order_id_' + i)[0].value;
@@ -22,6 +22,7 @@ var bills = {
 	    price.innerHTML = toMoney(bills.getOrderPrice(orderId));
 	}
 	bills.sumColumn();
+*/
     },
 
     sumColumn: function()
@@ -36,55 +37,6 @@ var bills = {
 	bills.setTotalPrice(sum);
     },
 
-    makeOrderSelect: function(value) {
-	var sel = document.createElement("select");
-	sel.name = "orders[" + $('#bill')[0].table_lines+"][order_id]";
-	sel.id = "order_id_"+ $('#bill')[0].table_lines;
-        sel.line_id = $('#bill')[0].table_lines;
-
-        for (var i=0; i<LODO.orderList.length; i++) {
-	    sel.add(new Option("" + LODO.orderList[i].order.customer.name + " - " + LODO.orderList[i].order.order_date + " - #" + LODO.orderList[i].order.id,
-                               i), null);
-	}
-
-	sel.onchange = function (event) {
-            $.each(
-                LODO.orderList[this.value].order.order_items,
-                function (order_item) {
-                    bills.addProduct($("#order_details_" + this.line_id), {price: 0, amount: 0, discount: 0.0, order_item: order_item});
-                }
-            )
-        }
-	sel.value = value;
-
-	return sel;
-    },
-
-    makeDiscount: function (value) {
-	var res = document.createElement("input");
-	res.type="text";
-	res.id='discount_' + $('#bill')[0].table_lines;
-	res.setAttribute("autocomplete","off");
-	
-	res.onkeyup = function (event) {bills.validate();}
-	res.value = value;
-	return res;
-    },
-    
-    makePrice: function (value) {
-	
-	var res = document.createElement("span");
-
-	var inp = document.createElement("input");
-	inp.type="hidden";
-	inp.id='price_' + $('#bill')[0].table_lines;
-	inp.name = "orders[" + $('#bill')[0].table_lines+"][price]";
-	inp.value = value;
-	res.appendChild(inp);
-	res.appendChild(bills.makeText('priceLabel'));
-	return res;
-    },
-
     setTotalPrice: function(price) {
 	price = price*(100.0-parseFloatNazi($('#discount_total')[0].value))*0.01;
 	$('#dynfield_sum')[0].innerHTML = toMoney(price);
@@ -96,31 +48,66 @@ var bills = {
 	$('#priceLabel_'+line)[0].innerHTML = toMoney(value);
     },
 
-    makeText: function(id, text, context) {
+
+
+
+/* MY CODE */
+    getCurrentLineId: function (context) {
+        var ctx = $(context);
+        return ctx[0].getAttribute('id') + "_" + (ctx[0].table_lines-1);
+    },
+
+    getCurrentLineName: function (context) {
+        var ctx = $(context);
+        return ctx[0].name + "[" + (ctx[0].table_lines-1) + "]";
+    },
+
+    addLine: function (context) {
+        var ctx = $(context);
+        var rows = bills.getRowList(ctx);
+
+        ctx[0].table_lines++;
+	return rows.insertRow(rows.rows.length);
+    },
+
+    getRowList: function (context) {
+        var ctx = $(context);
+        return $i(ctx[0].getAttribute('id') + '_rows', ctx)[0];
+    },
+
+
+
+    makeDiscount: function (value, context) {
+	var res = document.createElement("input");
+	res.type = "text";
+	res.id = bills.getCurrentLineId(context) + '_discount';
+	res.name = bills.getCurrentLineName(context) + '[discount]';
+	res.setAttribute("autocomplete","off");
+	
+	res.onkeyup = function (event) {bills.validate();}
+	res.value = value;
+	return res;
+    },
+    
+    makePrice: function (value, context) {	
 	var res = document.createElement("span");
-	res.id=id + "_" + $(context)[0].table_lines;
-        if (text) {
-          $(res).html(text);
-        }
+
+	var inp = document.createElement("input");
+	inp.type = "hidden";
+	inp.id = bills.getCurrentLineId(context) + '_price';
+	inp.name = bills.getCurrentLineId(context) + '[price]';
+	inp.value = value;
+	res.appendChild(inp);
+	res.appendChild(bills.makeText('priceLabel'));
 	return res;
     },
 
-    makeOrderDetails: function(lines) {
-	var res = makeTemplateInstance("order_details_" + $('#bill')[0].table_lines, "order_template")
-	
-	res.updateProducts = function (order_nr) {
-            this.discount = LODO.orderList[order_nr].order.price / sum(map(function (x) { return x.price; }, LODO.orderList[order_nr].order.order_items));
-
-            $.each(
-                LODO.orderList[order_nr].order.order_items,
-                function (order_item) {
-                    bills.addProduct(this, {price: LODO.orderList[order_nr].order.price * discount, amount: 0, discount: 0.0, order_item: order_item});
-                }
-            )
-
-        }
-        res.productLines = 0;
-        return res;
+    makeText: function(id, text, context) {
+	var res = document.createElement("span");
+	res.id = bills.getCurrentLineId(context) + '_' + id;
+        if (text)
+          $(res).html(text);
+	return res;
     },
 
     addCell: function (row, content) {
@@ -129,63 +116,104 @@ var bills = {
         return c;
     },
 
-    addProduct: function (order_details, line) {
-        // bills.addProduct($("#order_details_" + this.line_id), {amount: 0, discount: 0.0, order_item: order_item});
 
-        var opTable = $("#" + order_details.getAttribute('id') + "_products", order_details)[0];
-	var row = opTable.insertRow(opTable.rows.length);
+    addProduct: function (line, context) {
+	var row = bills.addLine(context);
     
-	bills.addCell(row, bills.makeText('product', line.order_item.product.name, opTable));
-	bills.addCell(row, bills.makeText('unit_price', line.price, opTable));
-	bills.addCell(row, bills.makeText('remaining', line.order_item.amount, opTable));
-
-/*
-	<th>Product</th>
-	<th>Unit Price</th>
-	<th>Remaining</th>
-	<th>Amount</th>
-	<th>Discount</th>
-	<th>Total Price</th>
-*/
-
-	order_details.productLines++;
+	bills.addCell(row, bills.makeText('product', line.order_item.product.name, context));
+	bills.addCell(row, bills.makeText('unit_price', line.price, context));
+	bills.addCell(row, bills.makeText('remaining', line.order_item.amount, context));
+	bills.addCell(row, bills.makeText('amount', line.order_item.amount, context));
+	bills.addCell(row, bills.makeDiscount(line.order_item.amount, context));
+	bills.addCell(row, bills.makeText('price', 4711, context));
 	stripe();
 	bills.validate();
    },
 
-    addOrder: function(line)
-    {
-	if(!$('#bill')[0].table_lines) {
-	    $('#bill')[0].table_lines = 0;
+    makeOrderSelect: function(value) {
+	var sel = document.createElement("select");
+	sel.id = bills.getCurrentLineId('#bill') + "_order_id";
+	sel.name = bills.getCurrentLineName('#bill') + "[order_id]";
+        sel.line_id = bills.getCurrentLineId('#bill')
+
+        for (var i=0; i<LODO.orderList.length; i++) {
+	    sel.add(new Option("" + LODO.orderList[i].order.customer.name + " - " + LODO.orderList[i].order.order_date + " - #" + LODO.orderList[i].order.id,
+                               i), null);
 	}
 
-	var opTable = $('#orders')[0];
-	var row = opTable.insertRow(opTable.rows.length);
+	sel.onchange = function (event) {
+            $i(sel.line_id + "_details")[0].updateProducts(this.value);
+        }
+	sel.value = value;
+
+	return sel;
+    },
+
+    makeOrderDetails: function(lines) {
+        console.log(bills.getCurrentLineId('#bill') + "_details");
+        console.log(bills.getCurrentLineName('#bill') + "[details]");
+
+	var res = makeTemplateInstance(
+            bills.getCurrentLineId('#bill') + "_details",
+            bills.getCurrentLineName('#bill') + "[details]",
+            "order_template");
+        console.log(res);
+
+	res.updateProducts = function (order_nr) {
+            console.log("updateProducts", order_nr, LODO.orderList[order_nr].order.order_items);
+            res.discount = LODO.orderList[order_nr].order.price / sum(map(function (x) { return x.price; }, LODO.orderList[order_nr].order.order_items));
+            res.table_lines = 0;
+
+            var rows = bills.getRowList(res);
+            while (rows.rows.length)
+                rows.deleteRow(0);
+
+            $.each(
+                LODO.orderList[order_nr].order.order_items,
+                function (order_item) {
+                    console.log("addProduct", order_item, LODO.orderList[order_nr].order.order_items[order_item]);
+                    bills.addProduct(
+                        {price: LODO.orderList[order_nr].order.price * this.discount,
+                         amount: 0,
+                         discount: 0.0,
+                         order_item: LODO.orderList[order_nr].order.order_items[order_item]},
+                        res);
+                })
+        }
+        return res;
+    },
+
+    addOrder: function(line) {
+	var row = bills.addLine('#bill');
     
-	bills.addCell(row, bills.makeOrderSelect(line?line.order_id:null));
+        var select = bills.makeOrderSelect(line?line.order_id:null);
+	bills.addCell(row, select);
 	var cell = bills.addCell(row, bills.makeOrderDetails(line?line.order_details:null));
-	/*
-	  If we are reediting an old line, insert the id so we can match them together
-	*/
+        select.onchange();
+
+	/* If we are reediting an old line, insert the id so we can match them together */
 	if (line) {
 	    var line_id = document.createElement("input");
 	    line_id.type = "hidden";
-	    line_id.name = "orders[" + $('#bill')[0].table_lines+"][id]";
+	    line_id.name = bills.getCurrentLineName('#bill') + "[id]";
 	    line_id.value = line.id;
 	    cell.appendChild(line_id);
 	}
-	
-	$('#bill')[0].table_lines++;
+
 	stripe();
 	bills.validate();
     },
 
-    addPredefined: function(){
+    addPredefined: function() {
+	$('#bill')[0].table_lines = 0;
+	$('#bill')[0].name = 'bill';
+/*
 	lines = LODO.billItemList;
 	for (var i=0; i<lines.length; i++) {
 	    line = lines[i]['bill_item'];
 	    bills.addOrder(line);
 	}
+*/
     }
 
 }
