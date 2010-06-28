ccded0ce928dcf6cd58c6c41b1772b6c6f080507
Ivar Nymoen <ivar.nymoen@gmail.com>
2010-06-28 16:27:28 +0200

    user_roles should return the roles assigned for current_company

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -5,4 +5,21 @@ class UserTest < ActiveSupport::TestCase
   test "the truth" do
     assert true
   end
+
+  test "ensure we see the proper roles" do
+    c1 = Company.make
+    dummy_company = Company.make
+    u = User.make(:current_company => c1)
+    r1 = Role.make(:name => "test_role")
+    r2 = Role.make(:name => "testrole_2")
+    dummy_role = Role.make(:name => "dummy")
+    Assignment.create!(:user => u, :company => c1, :role => r1)
+    Assignment.create!(:user => u, :company => c1, :role => r2)
+    Assignment.create!(:user => u, :company => dummy_company, :role => dummy_role)
+
+    u.reload
+    assert_equal "test_role::testrole_2", u.role_symbols.sort_by(&:to_s).join("::")
+  end
+
 end
+
